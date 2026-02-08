77c2d9215ede7baafc606dff37a723e6857c82d4
Atik1000 <foysalkazi45@gmail.com>
2026-02-08 12:50:40 +0600

    feat(ui): add movie card and movies grid components

---
+++ components/movies-grid.tsx
@@ -0,0 +1,23 @@
+import { MoviesGridProps } from '@/types/components';
+import { MovieCard } from './movie-card';
+
+export function MoviesGrid({
+  movies,
+  watchlist,
+  onWatchlistToggle,
+  showRemoveButton = false,
+}: MoviesGridProps) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center sm:justify-items-stretch">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          isInWatchlist={watchlist.includes(movie.id)}
+          onWatchlistToggle={onWatchlistToggle}
+          showRemoveButton={showRemoveButton}
+        />
+      ))}
+    </div>
+  );
+}
